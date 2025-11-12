3948408f3ab01325777d58ba9461148d5a511cb7
Volha <kresiko@gmail.com>
2025-11-12 15:04:49 +0200

    Auth creation started. The problem is with injection of UserRepository into CustomUserDetailsService

--- src/main/java/com/volia/example/userservice/model/User.java
+++ src/main/java/com/volia/example/userservice/model/User.java
@@ -35,26 +35,13 @@ public abstract class User {
     @Column(unique = true)
     @ToString.Include
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password; // excluded from toString()
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @ToString.Include
+    private Role role;
 }
-
-
-//CONSTRAINTS ON USING DATA
-// Add a required-args constructor (not useful for JPA)
-//        Potential Issues with @Data on JPA entities
-//equals() / hashCode():
-//
-//These methods are generated based on all fields, which can cause issues for:
-//
-//Lazy-loaded relationships
-//
-//Entity identity (e.g. comparing entities before id is set)
-//
-//Best practice: Use @EqualsAndHashCode(onlyExplicitlyIncluded = true) and include only id.
-//
-//toString():
-//
-//It can trigger lazy loading and stack overflows if there are bidirectional relationships.
-//
-//Better to use @ToString(onlyExplicitlyIncluded = true) or avoid @Data.
-//
-
